557f92906eb2a2cbd98adbfd081904bebda49af0
Vytautas Lozickas <vytautas.lozickas@nfq.lt>
2018-11-07 00:02:52 +0200

    Implement first react component #workshop-1

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+import { App } from './root/App';
+
+render(<App />, document.getElementById('app'));
---
+++ src/root/App.jsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export class App extends React.Component {
+    render() {
+        return (
+            <h1>Hello, world!</h1>
+        );
+    }
+}
