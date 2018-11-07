d7b20277b691b2602bee40891712c1774fa14c13
VytenisK <vytenis.kuciauskas.ext@home24.de>
2018-11-07 11:13:08 +0200

    Export default

--- src/root/App.jsx
+++ src/root/App.jsx
@@ -1,9 +1,8 @@
 /* eslint-disable react/prefer-stateless-function */
-/* eslint-disable import/prefer-default-export */
 import * as React from 'react';
 
-export class App extends React.Component {
+export default class App extends React.Component {
   render() {
-    return <h1>Hello, world!</h1>;
+    return <h1 className="asdasd">Hello, world!</h1>;
   }
 }
