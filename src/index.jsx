04d0fafc6bad6b4bbe7dc9267694197b89e5cca9
VytenisK <vytenis.kuciauskas.ext@home24.de>
2018-11-07 10:21:35 +0200

    Updates

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import { App } from './root/App';
+
+import App from './root/App';
 
 render(<App />, document.getElementById('app'));
--- src/root/App.jsx
+++ src/root/App.jsx
@@ -1,9 +1,3 @@
 import * as React from 'react';
 
-export class App extends React.Component {
-    render() {
-        return (
-            <h1>Hello, world!</h1>
-        );
-    }
-}
+export default () => <div>Hello</div>;
