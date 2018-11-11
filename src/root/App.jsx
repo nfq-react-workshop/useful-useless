6156879ab57e8a75fd2787d746d50848eec27f7a
Irmantas Zenkus <irmiuz@gmail.com>
2018-11-11 13:40:51 +0200

    configs

---
+++ src/root/App.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/prefer-stateless-function */
+import * as React from 'react';
+
+export default class App extends React.Component {
+    render() {
+        return <h1 className="asdasd">Hello, world!</h1>;
+    }
+}
