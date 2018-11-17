6a0d3fff4e28f202936159864db7fe72067d0e1e
Irmantas Zenkus <irmiuz@gmail.com>
2018-11-17 17:40:37 +0200

    logo

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,8 +4,9 @@ import Nav from './Nav';
 const Menu = () => (
     <nav className="navbar" role="navigation">
         <div className="navbar-brand">
-            <a href="/" className="navbar-item">
-                € LOGO $
+            <a href="/" className="navbar-item has-text-primary">
+                <i className="fab fa-hacker-news" />
+                &nbsp;HN
             </a>
             <a href="#" role="button" className="navbar-burger burger">
                 <span aria-hidden="true" />
