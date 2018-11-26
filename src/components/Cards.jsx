4d0fff01fded41c05c99ea6940fa20f721b69119
Irmantas Zenkus <irmiuz@gmail.com>
2018-11-26 13:48:33 +0200

    cards fetch from API

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,27 +1,62 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import Card from './Card';
+import { withRouter } from 'react-router';
 
-const items = [
-    {
-        title: 'Making rain simulation as real as possible',
-        comments_count: 14,
-        url: 'http://rainbowhunt.me/?plays',
-        user: 'sazers',
-    },
-    {
-        title: 'Show HN: Google Earth for live radios',
-        comments_count: 1,
-        url: 'http://radio.garden',
-        user: 'jaoued',
-    },
-];
+class Cards extends React.Component {
+    constructor(props) {
+        super(props);
 
-const Cards = () => (
-    <div className="">
-        {items.map((item, index) => (
-            <Card key={item.url} {...item} number={index + 1} />
-        ))}
-    </div>
-);
+        this.fetchItems = this.fetchItems.bind(this);
+        this.receiveItems = this.receiveItems.bind(this);
 
-export default Cards;
+        this.state = {
+            items: [],
+        };
+    }
+
+    componentDidMount() {
+        this.fetchItems();
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.match.params.page !== this.props.match.params.page) {
+            this.fetchItems();
+        }
+    }
+
+    render() {
+        return this.state.isFetching ? (
+            <div>is loading</div>
+        ) : (
+            <div className="">
+                {this.state.items.map((item, index) => (
+                    <Card key={item.url} {...item} number={index + 1} />
+                ))}
+            </div>
+        );
+    }
+
+    fetchItems() {
+        const page = Number(this.props.match.params.page) || 1;
+        this.setState({ isFetching: true });
+        fetch(`https://api.hnpwa.com/v0/news/${page}.json`)
+            .then(response => response.json())
+            .then(this.receiveItems);
+    }
+
+    receiveItems(items) {
+        this.setState({
+            items,
+            isFetching: false,
+        });
+    }
+}
+
+Cards.propTypes = {
+    match: PropTypes.any,
+    location: PropTypes.any,
+    history: PropTypes.any,
+};
+
+export default withRouter(Cards);
